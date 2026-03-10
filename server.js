19fd6515d880ff4b73eef1c3c05e2951125314be
project279 <24it25@sdpc.ac.in>
2026-03-10 12:55:26 +0530

    fixed groq model

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ messages: [
 { role: "system", content: "You are a helpful AI chatbot." },
 { role: "user", content: message }
 ],
-model: "llama3-8b-8192"
+model: "llama-3.3-70b-versatile"
 });
 
 const reply = completion.choices[0].message.content;
